7aaee4ca944947a30bf7d8790793c3190746b407
Mai Meo <duyanh1202tm@gmail.com>
2021-12-30 14:50:43 +0700

    fix apigateway method

--- src/handlers/listener.js
+++ src/handlers/listener.js
@@ -1,40 +1,40 @@
-const {SendMessageCommand, SQSClient} = require( "@aws-sdk/client-sqs");
+const { SendMessageCommand, SQSClient } = require("@aws-sdk/client-sqs");
 
 const QUEUE_URL = process.env.QUEUE_URL;
 
 // Set the AWS Region.
-const REGION = "us-east-1"
+const REGION = "us-east-1";
 // Create SQS service object.
 const sqsClient = new SQSClient({ region: REGION });
 
+exports.index = async (event) => {
+  const { id, title, product_type, created_at } = event;
 
-const params = {
-    MessageBody: "Information about current NY Times fiction bestseller for week of 12/11/2016.",
-    // MessageDeduplicationId: "TheWhistler",  // Required for FIFO queues
-    // MessageGroupId: "Group1",  // Required for FIFO queues
-    QueueUrl: QUEUE_URL //SQS_QUEUE_URL; e.g., 'https://sqs.REGION.amazonaws.com/ACCOUNT-ID/QUEUE-NAME'
+  const eventBody = { id, title, product_type, created_at };
+
+  const params = {
+    MessageBody: JSON.stringify(eventBody),
+    MessageDeduplicationId: "TheWhistler", // Required for FIFO queues
+    MessageGroupId: "Group1", // Required for FIFO queues
+    QueueUrl: QUEUE_URL, //SQS_QUEUE_URL; e.g., 'https://sqs.REGION.amazonaws.com/ACCOUNT-ID/QUEUE-NAME'
   };
 
-exports.index = async (event) => {
-    console.log('VAO LISTENER');
-    console.log("QUEUE_URL",QUEUE_URL);
+  try {
+    const data = await sqsClient.send(new SendMessageCommand(params));
+    console.log("Success, message sent. MessageID:", data.MessageId);
+    return data; // For unit tests.
+  } catch (err) {
+    console.log("Error", err);
+  }
 
-    try {
-        const data = await sqsClient.send(new SendMessageCommand(params));
-        console.log("Success, message sent. MessageID:", data.MessageId);
-        return data; // For unit tests.
-      } catch (err) {
-        console.log("Error", err);
-      }
+  let responseBody = {
+    message: "Push message successfully",
+  };
 
-    let responseBody = {
-        message: 'Push message successfully',
-    };
+  let response = {
+    statusCode: 200,
+    body: JSON.stringify(responseBody),
+  };
 
-    let response = {
-        statusCode: 200,
-        body: JSON.stringify(responseBody)
-    };
-
-    return response;
-}
+  return response;
+};
