aa92104b941a3dd18ea10c004ac424f72a3cc7be
Mai Meo <maimeo@Mais-MacBook-Air.local>
2021-12-30 09:09:05 +0700

    get queue url in template.yml file

--- src/handlers/listener.js
+++ src/handlers/listener.js
@@ -1,13 +1,17 @@
 const AWS = require('aws-sdk');
 AWS.config.update({ region: 'us-east-1' });
+const QUEUE_URL = process.env.QUEUE_URL;
 
 exports.index = async (event) => {
     console.log('VAO LISTENER');
+    console.log("QUEUE_URL",QUEUE_URL);
 
     const sqs = new AWS.SQS({apiVersion: '2012-11-05'});
+
     let params = {
         QueueName: 'demo-trustana-queue'
     };
+    
     sqs.sendMessage(params, function(err, data) {
         if (err) {
             console.log("Error", err);
